ff6a36e06a9fee636d453d7c84883561a450b90d
d4v3-rm <scarano.salvatore.71@gmail.com>
2026-02-22 17:06:03 +0100

    fix(main): prefer unpacked ffmpeg binaries in packaged app

--- src/main/services/binary-path.utils.ts
+++ src/main/services/binary-path.utils.ts
@@ -1,21 +1,18 @@
 import fs from 'node:fs';
 
 const ASAR_SEGMENT_PATTERN = /([\\/])app\.asar([\\/])/;
+const toUnpackedAsarPath = (binaryPath: string): string =>
+  binaryPath.replace(ASAR_SEGMENT_PATTERN, '$1app.asar.unpacked$2');
 
 export const resolveBundledBinaryPath = (binaryPath: string | null | undefined): string | null => {
   if (!binaryPath) {
     return null;
   }
 
-  if (fs.existsSync(binaryPath)) {
-    return binaryPath;
+  if (ASAR_SEGMENT_PATTERN.test(binaryPath)) {
+    const unpackedBinaryPath = toUnpackedAsarPath(binaryPath);
+    return fs.existsSync(unpackedBinaryPath) ? unpackedBinaryPath : null;
   }
 
-  const unpackedBinaryPath = binaryPath.replace(ASAR_SEGMENT_PATTERN, '$1app.asar.unpacked$2');
-
-  if (unpackedBinaryPath !== binaryPath && fs.existsSync(unpackedBinaryPath)) {
-    return unpackedBinaryPath;
-  }
-
-  return null;
+  return fs.existsSync(binaryPath) ? binaryPath : null;
 };
